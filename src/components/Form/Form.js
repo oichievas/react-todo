dbb440f1cd21be3ec50552f703b8149b0e8f1bf3
oichievas <oichievas>
2022-10-10 11:14:46 +0600

    initial commit

---
+++ src/components/Form/Form.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './Form.css';
+
+const Form = (props) => {
+  const [value, setValue] = useState("")
+
+  return (
+    <form className='form'
+      onSubmit={e => {
+      e.preventDefault();
+      props.putTodo(value);
+      setValue("");
+    }}>
+      <input type="text" placeholder="Bведите текст" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  );
+};
+
+export default Form;
